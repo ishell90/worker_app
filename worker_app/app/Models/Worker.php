9dadd792c8897ef3a57ae1750dce5cdf96dbdda1
Ilya Shalaev <iashalaev@monamy.app>
2024-06-07 18:27:08 +0300

    добавил табличку и модельку к ней

---
+++ worker_app/app/Models/Worker.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Worker extends Model
+{
+    use HasFactory;
+
+    public $table = 'workers';
+
+    protected $fillable = [
+        'name',
+        'surname',
+        'email',
+        'text',
+        'is_married',
+        'age',
+    ];
+}
